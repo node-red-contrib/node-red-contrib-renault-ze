a95b37bfd59a1e7915f4d59c1307bfdfcf1b9b55
Konrad Mattheis <konne@users.noreply.github.com>
2020-11-14 21:33:55 +0100

    * extract script from html

---
+++ src/renault-ze.html.ts
@@ -0,0 +1,22 @@
+import {EditorRED} from "node-red";
+
+declare var RED: EditorRED;
+
+RED.nodes.registerType('renault-ze',{
+    category: 'Renault',
+    color: '#a6bbcf',
+    defaults: {
+        name: {value:""}
+    },
+    inputs:1,
+    outputs:1,
+    icon: "file.png",
+    label: function() {
+        return this.name||"renault-ze";
+    },
+    credentials: {
+        username: {type:"text"},
+        password: {type:"password"}
+    },
+    //settings: ({} as any)
+});
